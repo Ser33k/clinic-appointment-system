74e0efd0540ad98b06f08dd8d92800b6e4aebbcb
Mikolaj <mikus1001@gmail.com>
2021-12-14 20:48:38 +0100

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">© 2020 Mikolaj Kiernikowski.</p>
+    </footer>
+  );
+}
+
+export default Footer;
